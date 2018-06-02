131359240efef827640ee0cf6ba425fe045cba1c
DidelotK <didelotkev@gmail.com>
2018-06-02 17:36:18 +0200

    Add http and https to firewall

--- k8s-cluster/firewall.tf
+++ k8s-cluster/firewall.tf
@@ -9,3 +9,21 @@ module "fw_ssh" {
   protocol    = "tcp"
   ports       = ["22"]
 }
+
+module "fw_http" {
+  source      = "../terraform-modules/firewall/ingress-allow"
+  name        = "allow-http"
+  description = "Allow HTTP for everyone"
+  network     = "${module.k8s_network.name}"
+  protocol    = "tcp"
+  ports       = ["80"]
+}
+
+module "fw_https" {
+  source      = "../terraform-modules/firewall/ingress-allow"
+  name        = "allow-https"
+  description = "Allow HTTPS for everyone"
+  network     = "${module.k8s_network.name}"
+  protocol    = "http"
+  ports       = ["443"]
+}
